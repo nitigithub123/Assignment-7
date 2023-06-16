59528dc74fab87abcf82f07fb8753f163d69bfc0
nitigithub123 <130463009+nitigithub123@users.noreply.github.com>
2023-06-16 21:25:26 +0530

    Add files via upload

---
+++ ques1.py
@@ -0,0 +1,13 @@
+def Calculate_GST(org_cost, N_price):
+  
+    # return value after calculate GST%
+    return (((N_price - org_cost) * 100) / org_cost);
+  
+# Driver program to test above functions
+org_cost = 100
+N_price = 120
+print("GST = ",end='')
+  
+print(round(Calculate_GST(org_cost, N_price)),end='')
+  
+print("%")
